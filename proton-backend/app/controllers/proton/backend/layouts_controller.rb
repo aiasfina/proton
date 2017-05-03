6727a2e898b2303c4d8d8da06f25c6470dc19122
aiasfina <aiasfina@gmail.com>
2017-05-04 04:25:13 +0800

    Update and create layout

--- proton-backend/app/controllers/proton/backend/layouts_controller.rb
+++ proton-backend/app/controllers/proton/backend/layouts_controller.rb
@@ -2,6 +2,8 @@ require_dependency "proton/backend/application_controller"
 
 module Proton::Backend
   class LayoutsController < ApplicationController
+    before_action :set_layout, except: [:index, :new, :create]
+
     def index
       @layouts = Proton::Layer::Layout.page params[:page]
     end
@@ -11,16 +13,23 @@ module Proton::Backend
     end
 
     def create
-      @layout = Proton::Layer::Layout.new
+      @layout = Proton::Layer::Layout.new params.require(:layout).permit!
+      @layout.save
     end
 
     def edit
     end
 
     def update
+      @layout.update params.require(:layout).permit!
     end
 
     def destroy
+    end
+
+    private
+    def set_layout
+      @layout = Proton::Layer::Layout.find params[:id]
     end
   end
 end
